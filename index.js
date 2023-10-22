4bad516b4727f0d826caddf40d87bdca560029f1
toufiqcse <toufiqcse7@gmail.com>
2023-10-22 21:22:07 +0600

    api json update

--- index.js
+++ index.js
@@ -4,20 +4,33 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// add midleware
 app.use(cors());
-// GET Req
+
 const projects = require("./data/projects.json");
 app.get("/projects", (req, res) => {
   res.send(projects);
 });
 
-// GET Req
 const aboutTeamMember = require("./data/aboutTeam.json");
 app.get("/aboutTeam", (req, res) => {
   res.send(aboutTeamMember);
 });
 
+// get id wise data from tours
+
+// app.get("/aboutTeam/:id", (req, res) => {
+//   const memberId = req.params.id;
+//   const teamMemberData = aboutTeamMember.find(
+//     (member) => member.id === memberId
+//   );
+
+//   if (teamMemberData) {
+//     res.send(teamMemberData);
+//   } else {
+//     res.status(404).send("Team member not found");
+//   }
+// });
+
 app.listen(port, () => {
-  console.log(`Portfolio server running on port ${port} `);
+  console.log(`Portfolio server running on port ${port}`);
 });
